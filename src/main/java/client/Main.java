e2aa071030dd1b9012439fe7b9cca8e1259427ef
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-03-06 20:25:26 +0100

    Create a server that allows clients to connect and send messages

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,4 +1,28 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 23456;
+
+    public static void main(String[] args) {
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())
+        ) {
+            String message = "Give me a record # 12";
+
+            System.out.println("Client started!");
+            outputStream.writeUTF(message);
+            System.out.printf("Sent: %s\n", message);
+            System.out.printf("Received: %s", dataInputStream.readUTF());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -3,19 +3,46 @@ package server;
 import server.data.CommandController;
 import server.data.JsonDatabase;
 import server.data.JsonDatabaseArray;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
+    private static final int PORT = 23456;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        JsonDatabase jsonDatabase = new JsonDatabaseArray(100);
-        CommandController commandController = new CommandController(jsonDatabase);
+//        Scanner scanner = new Scanner(System.in);
+//        JsonDatabase jsonDatabase = new JsonDatabaseArray(100);
+//        CommandController commandController = new CommandController(jsonDatabase);
+//
+//        String command = scanner.nextLine();
+//        while (!command.equals("exit")) {
+//            commandController.executeCommand(command);
+//
+//            command = scanner.nextLine();
+//        }
 
-        String command = scanner.nextLine();
-        while (!command.equals("exit")) {
-            commandController.executeCommand(command);
+        System.out.println("Server started!");
 
-            command = scanner.nextLine();
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            try (
+                    Socket socket = serverSocket.accept();
+                    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())
+            ) {
+                String message = inputStream.readUTF();
+                System.out.printf("Received: %s\n", message);
+
+                String sendMessage = "A record # 12 was sent!";
+                outputStream.writeUTF(sendMessage);
+                System.out.printf("Sent: %s", sendMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
